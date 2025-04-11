11c182d286ba8c052d780445b5b61f896a4e912a
Nuralim <n_abish@kbtu.kz>
2025-04-11 18:04:01 +0500

    Переопределил метод сохранения модели

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,15 @@
-from django.contrib import admin
+import os
 
-from .models import  Contact
+import requests
+from django.contrib import admin, messages
+from .models import Contact
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+INSTANCE = os.getenv('GREEN_API_INSTANCE_ID')
+TOKEN    = os.getenv('GREEN_API_TOKEN')
 
 # Register your models here.
 @admin.register(Contact)
@@ -9,4 +18,29 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('name', 'phone')
+
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+
+        if not change:
+
+            url = f"https://api.green-api.com/waInstance{INSTANCE}/sendMessage/{TOKEN}"
+            chat_id = f"{obj.phone}@c.us"
+            payload = {
+                "chatId": chat_id,
+                "message": f"Здравствуйте, {obj.name}! Ваши данные успешно сохранены."
+            }
+
+            try:
+                resp = requests.post(url, json=payload, timeout=10)
+                resp.raise_for_status()
+            except requests.RequestException as e:
+                self.message_user(
+                    request,
+                    f"Не удалось отправить сообщение через Green API: {e}",
+                    level=messages.ERROR,
+                )
+
 
