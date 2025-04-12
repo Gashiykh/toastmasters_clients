3f09caa62c06a250cc71610038f7f85c65c868f8
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-04-12 11:15:57 +0500

    init

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 echo "=== Запуск миграций ==="
 python manage.py migrate --noinput
 
+echo "=== Статики ==="
+python manage.py collectstatic --noinput
+
+
 echo "=== Запуск сервера ==="
 gunicorn core.wsgi:application --bind 0.0.0.0:8001 --workers 3
