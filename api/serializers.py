d2c0b276f1a07bfe42b27ddd089cd1ba73a5d10d
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-04-24 19:05:53 +0500

    init

--- api/serializers.py
+++ api/serializers.py
@@ -25,3 +25,28 @@ class ContactModelSerializer(serializers.ModelSerializer):
             })
         return data
 
+
+class BroadcastSerializer(serializers.Serializer):
+    message = serializers.CharField(max_length=2000, required=False)
+
+    def validate(self, data):
+        if not data.get('message'):
+            data['message'] = DEFAULT_TEMPLATE
+        return data
+
+DEFAULT_TEMPLATE = """Dear {name},
+Only 2 days to go until *Nomads of the Digital Era: Speak, Lead, Innovate* —
+the very first international Toastmasters conference in Kazakhstan! 🇰🇿✨
+
+📌 *Event Details*
+• Date: April 26, 2025
+• Format: In-person Conference
+• Location: KIMEP University, Almaty
+
+We can’t wait to welcome you to a day full of powerful speeches, leadership insights, and meaningful connections. 💬🤝
+
+✨ Tomorrow we’ll share the full agenda and practical info.
+🔗 The conference link will be sent on the event day.
+
+Get ready to be inspired!
+"""
