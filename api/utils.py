928789e60c49a0f9a522e9666e80b95a557caa95
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-04-24 19:06:42 +0500

    init

---
+++ api/utils.py
@@ -0,0 +1,19 @@
+import os, requests
+from asyncio import timeout
+
+INSTANCE = os.getenv("GREEN_API_INSTANCE_ID")
+TOKEN    = os.getenv("GREEN_API_TOKEN")
+BASE_URL = f"https://api.green-api.com/waInstance{INSTANCE}/sendMessage/{TOKEN}"
+
+
+def normalize_phone(number):
+    return number.replace("+", "").replace(" ", "").strip() + "@c.us"
+
+def send_whatsapp(chat_id, message):
+    resp = requests.post(
+        BASE_URL,
+        json={"chatId": chat_id, "message": message},
+        timeout=10
+    )
+
+    resp.raise_for_status()
