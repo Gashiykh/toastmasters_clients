18f8188ca6eeeb727f3c1f973fd5401ac09fcf5d
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-04-25 13:27:01 +0500

    add model in admin

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,7 @@
 import os
 
-import requests
-from django.contrib import admin, messages
-from .models import Contact
+from django.contrib import admin
+from .models import Contact, Telephone
 
 from dotenv import load_dotenv
 
@@ -11,7 +10,6 @@ load_dotenv()
 INSTANCE = os.getenv('GREEN_API_INSTANCE_ID')
 TOKEN = os.getenv('GREEN_API_TOKEN')
 
-# Register your models here.
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     model = Contact
@@ -19,28 +17,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'visit_form',)
     search_fields = ('name', 'phone')
 
-
-    def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
-
-
-        if not change:
-
-            url = f"https://api.green-api.com/waInstance{INSTANCE}/sendMessage/{TOKEN}"
-            chat_id = f"{obj.phone}@c.us"
-            payload = {
-                "chatId": chat_id,
-                "message": f"Здравствуйте, {obj.name}! Ваши данные успешно сохранены."
-            }
-
-            try:
-                resp = requests.post(url, json=payload, timeout=10)
-                resp.raise_for_status()
-            except requests.RequestException as e:
-                self.message_user(
-                    request,
-                    f"Не удалось отправить сообщение через Green API: {e}",
-                    level=messages.ERROR,
-                )
-
+@admin.register(Telephone)
+class TelephoneAdmin(admin.ModelAdmin):
+    model = Telephone
+    list_display = ('name', 'phone')
+    search_fields = ('name', 'phone')
 
