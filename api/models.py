6c34e51c959b6030f4dbdbd4ec443c0f5c27b9a1
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-04-25 13:26:24 +0500

    add new model

--- api/models.py
+++ api/models.py
@@ -22,3 +22,15 @@ class Contact(models.Model):
     class Meta:
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
+
+
+class Telephone(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    phone = models.CharField("Телефон", max_length=20)
+
+    def __str__(self):
+        return f"{self.name} ({self.phone})"
+
+    class Meta:
+        verbose_name = 'Телефон'
+        verbose_name_plural = 'Телефоны'
